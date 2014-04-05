dc0a7ff324c258716932b52947d089fc437177cc
krypton9208 <krypton9208@gmail.com>
2014-04-06 00:10:19 +0200

    Start

---
+++ Lab 2/Form1.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace Lab_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Tabele = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AccessibleRole = System.Windows.Forms.AccessibleRole.MenuBar;
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToOrderColumns = true;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Tabele});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 30);
+            this.dataGridView1.MultiSelect = false;
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersWidth = 50;
+            this.dataGridView1.Size = new System.Drawing.Size(176, 150);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            // 
+            // Tabele
+            // 
+            this.Tabele.HeaderText = "Nazwy Tabel";
+            this.Tabele.Name = "Tabele";
+            this.Tabele.ReadOnly = true;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.AllowUserToOrderColumns = true;
+            this.dataGridView2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(194, 30);
+            this.dataGridView2.MultiSelect = false;
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.Size = new System.Drawing.Size(578, 150);
+            this.dataGridView2.TabIndex = 1;
+            this.dataGridView2.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView2_CellClick);
+            this.dataGridView2.RowsAdded += new System.Windows.Forms.DataGridViewRowsAddedEventHandler(this.dataGridView2_RowsAdded);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(293, 207);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(139, 23);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "Zapisz dane na serwerze";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(17, 189);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Tabela:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(79, 190);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(15, 212);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Rekord:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(79, 212);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 13);
+            this.label4.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 246);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "SQL Simple Project";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Tabele;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Lab 2/Form1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Lab_2
+{
+    
+    public partial class Form1 : Form
+    {
+        Panel[] panels = new Panel[100];
+        public string[] tab1;   
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Bazadanych siema = new Bazadanych(@"DAMIAN\SQLS", @"Northwind");
+            string[] t = new string[1];
+            t = siema.GetTableNames();
+            foreach (var item in t)
+            {
+                dataGridView1.Rows.Add(item.ToString());
+            }
+            dataGridView2.DataSource = siema.GetTableData(t[1]);
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            Bazadanych siema = new Bazadanych(@"DAMIAN\SQLS", @"Northwind");
+            dataGridView2.DataSource = null;
+            dataGridView2.Rows.Clear();
+            string temp = dataGridView1.Rows[e.RowIndex].Cells[0].Value.ToString();
+            dataGridView2.DataSource =siema.GetTableData(temp);
+            label2.Text = temp;
+        }
+
+        
+
+        private void dataGridView2_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            label4.Text = Convert.ToString(dataGridView2.Rows[e.RowIndex].Index + 1);
+        }
+
+       
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Bazadanych siema = new Bazadanych(@"DAMIAN\SQLS", @"Northwind");
+            DataTable dv = (DataTable)(dataGridView2.DataSource);
+           // siema.SaveTableToServer(dv, label2.Text);
+           
+        }
+
+        private void dataGridView2_RowsAdded(object sender, DataGridViewRowsAddedEventArgs e)
+        {
+            Bazadanych siema = new Bazadanych(@"DAMIAN\SQLS", @"Northwind");
+            //siema.SaveRowToBase((DataTable)(dataGridView2.DataSource), label2.Text, e.RowIndex);
+        }
+    }
+
+    class Bazadanych
+    {
+        SqlConnection conn;
+        public Bazadanych(string ServerName, string DbName)
+        {
+            conn = new SqlConnection("Data Source=" + ServerName + ";" + "Initial Catalog=" + DbName + ";" + @"Trusted_Connection=true");
+            conn.Open();
+        }
+        public string[] GetTableNames()
+        {
+            DataTable Tables = conn.GetSchema("Tables");
+            List<string> TablesNames = new List<string>();
+            foreach (DataRow row in Tables.Rows)
+            {
+                if (row[3].ToString() == "BASE TABLE") TablesNames.Add(row[2].ToString());
+            }
+            string[] TablesS = new string[TablesNames.Count];
+            
+            int i = 0;
+            foreach (var item in TablesNames)
+            {
+                TablesS[i] = item.ToString();
+                i++;
+            }
+            return TablesS;
+        }
+        public DataTable GetTableData(string TableName)
+        {
+            DataTable t = new DataTable();
+            using (SqlDataAdapter da = new SqlDataAdapter("SELECT * FROM " + TableName, conn))
+            {
+                da.Fill(t);
+            };
+            return t;
+        }
+        public Boolean SaveRowToBase(DataTable Table, String TableName, int RowIndex)
+        {
+            using (SqlCommand comm = new SqlCommand())
+            {
+            string cmdString = "INSERT INTO " + TableName  + " (";
+            
+            for (int i = 0; i < Table.Columns.Count; i++)
+			{
+			    cmdString += Table.Columns[i].ColumnName + ", ";
+			}
+            cmdString += ") VALUES (";
+            for (int i = 0; i < Table.Columns.Count; i++)
+			{
+                if (i +1 == Table.Columns.Count) cmdString += Table.Columns[i].ToString() + " ";
+               
+                    //cmdString += Table.Row[RowIndex].Cell[i].Value.ToString() + ", ";
+			}
+            cmdString += ");";
+            //MessageBox.Show(cmdString);
+            
+            }
+            return true;
+        }
+    }
+}
